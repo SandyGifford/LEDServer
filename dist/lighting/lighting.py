dc4b3533fedb04c82ae1cb3e7b3fc5f076a0385d
Sandy Gifford <sanford.r.gifford@gmail.com>
2022-01-09 15:27:41 +0000

    basic arguments for lighting from CLI

--- dist/lighting/lighting.py
+++ dist/lighting/lighting.py
@@ -1,3 +1,4 @@
+import sys
 from utils.logging_utils import init_logging
 init_logging()
 
@@ -9,4 +10,18 @@ from routines.noise import noise
 from routines.rotate import rotate
 from routines.off import off
 
-from_db()
+routine = sys.argv[1] if len(sys.argv) >= 2 else None
+
+print(routine)
+
+if routine == None:
+	print("No routine specified, turning off")
+	off()
+elif routine == "from_db": from_db()
+elif routine == "pulse": pulse()
+elif routine == "swap": swap()
+elif routine == "static_rainbow": static_rainbow()
+elif routine == "noise": noise()
+elif routine == "rotate": rotate()
+elif routine == "off": off()
+else: print("No routine named " + "\"" + routine + "\"")
