8576b00080961ded26ed0daa92f732b36b345f94
Sandy Gifford <sanford.r.gifford@gmail.com>
2022-01-10 14:38:13 +0000

    arguments for cli routines

--- dist/lighting/lighting.py
+++ dist/lighting/lighting.py
@@ -10,18 +10,42 @@ from routines.noise import noise
 from routines.rotate import rotate
 from routines.off import off
 
-routine = sys.argv[1] if len(sys.argv) >= 2 else None
+routine_name = sys.argv[1] if len(sys.argv) >= 2 else None
+cli_args = sys.argv[2:]
 
-print(routine)
+routines = [from_db, pulse, swap, static_rainbow, noise, rotate, off]
 
-if routine == None:
+routine_map = {}
+
+for r in routines:
+	routine_map[r.__name__] = r
+
+def get_arg_names(fn):
+	return fn.__code__.co_varnames[:fn.__code__.co_argcount]
+
+def do_routine(routine_name):
+	routine = routine_map[routine_name]
+	routine_args = get_arg_names(routine)
+	call_args = {}
+
+	for a in range(0, len(cli_args)):
+		arg_name = routine_args[a]
+		split = cli_args[a].split(":")
+		arg_val = split[0]
+
+		if len(split) >= 2:
+			arg_name = split[0]
+			arg_val = split[1]
+
+		if "," in arg_val: arg_val = tuple(map(int, arg_val.split(",")))
+		else: arg_val = float(arg_val)
+
+		call_args[arg_name] = arg_val
+
+	routine(**call_args)
+
+if routine_name == None:
 	print("No routine specified, turning off")
 	off()
-elif routine == "from_db": from_db()
-elif routine == "pulse": pulse()
-elif routine == "swap": swap()
-elif routine == "static_rainbow": static_rainbow()
-elif routine == "noise": noise()
-elif routine == "rotate": rotate()
-elif routine == "off": off()
-else: print("No routine named " + "\"" + routine + "\"")
+elif routine_name not in routine_map: print("No routine named " + "\"" + routine_name + "\"")
+else: do_routine(routine_name)
